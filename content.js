ef4ebe2bd656ba5c227ede05d18b94c7ea118e61
ravish <ravishvishwakarma2010@gmail.com>
2025-07-12 01:30:05 +0530

    Icon problem fix

--- content.js
+++ content.js
@@ -140,10 +140,11 @@ function insertSkipButton() {
 
 
     btn.innerHTML = `
-    <svg xmlns="http:
-      <path d="M4 4v16l12-8zM16 4v16h2V4z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+        <path d="M4 4v16l12-8zM16 4v16h2V4z"/>
     </svg>
-  `;
+    `;
+
 
     btn.title = 'Skip Ad';
 
